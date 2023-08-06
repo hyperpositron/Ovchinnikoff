21e900a3ee2513562f0249c6ed783afd96a21237
Александр <hyperpositron@gmail.com>
2023-08-06 14:56:15 +0200

    класс  Employee метод mathodq() метод printNameAndPosition()

---
+++ src/Ovchinnikoff_2/hw_5/Employee.java
@@ -0,0 +1,27 @@
+package Ovchinnikoff_2.hw_5;
+
+public class Employee {
+    String name;
+    int salary;
+    int age;
+    String position;
+
+    public Employee(String name, int salary, int age, String position) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+        this.position = position;
+    }
+
+    void methodq() {
+        System.out.println("Имя : " + name);
+        System.out.println("Зарплата : " + salary);
+        System.out.println("Год рождения: " + age);
+        System.out.println("Должность: " + position);
+    }
+
+    void printNameAndPosition() {
+        System.out.println("Имя : " + name);
+        System.out.println("Должность : " + position);
+    }
+}
---
+++ src/Ovchinnikoff_2/hw_5/Main.java
@@ -0,0 +1,9 @@
+package Ovchinnikoff_2.hw_5;
+
+public class Main {
+public static void main(String[]args){
+    Employee employee = new Employee("Никита",20000,1992,"Пожарный");
+    employee.methodq();
+    employee.printNameAndPosition();
+}
+}
