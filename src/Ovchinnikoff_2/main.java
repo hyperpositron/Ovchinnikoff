15276f8bacb21dcf4023d63c9b92639891666fde
Александр <hyperpositron@gmail.com>
2023-08-06 13:43:02 +0200

    first commit

---
+++ src/Ovchinnikoff_2/main.java
@@ -0,0 +1,23 @@
+package Ovchinnikoff_2;
+
+public class main {
+    class employee {
+        String name;
+        int salary;
+        int age;
+        String position;
+
+        public employee(String name, int salary, int age, String position) {
+            this.name = name;
+            this.salary = salary;
+            this.age = age;
+            this.position = position;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
